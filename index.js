03898bbb5420cd8e19b78873c43b489105757d19
Viktor <viktorpar@gmail.com>
2018-08-12 18:38:15 +0300

    adde authentication and authorization

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
@@ -6,7 +7,14 @@ const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
 const app = express();
+
+if (!config.get('jwtPrivateKey')) {
+    console.log('FATAL ERROR: jwtPrivateKey not defined.');
+    process.exit(1);
+}
 
 mongoose
     .connect('mongodb://localhost/vildy')
@@ -19,6 +27,8 @@ app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3500;
 app.listen(port, console.log(`Listening port ${port}`));
